cf2f2e8672baeeb1040e69d11e1d7b52e3776767
Piotrek <@>
2017-10-25 22:50:51 +0200

    Przesłanie plików

---
+++ A_7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj 3 liczby, wprowadzenie każdej zakończ enterem.");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            double c = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Średnia arytmetyczna: " + (a + b + c) / 3);
+            Console.WriteLine("Średnia geometryczna: " + Math.Pow(a * b * c, 1.0 / 3));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Napisz program, który wczyta z klawiatury 3 liczby (typu double), a następnie wyświetli na konsoli ich średnią arytmetyczną i geometryczną.
